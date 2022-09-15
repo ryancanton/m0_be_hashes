4a3746ba05825879bb44c834690cc8bb39c109bc
Ryan Canton <ryancanton999@gmail.com>
2022-09-15 09:34:18 -0600

    Initial commit

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,29 +1,39 @@
 
-Activity:
+#Activity:
 
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your Terminal: 
+# file by entering the following command in your Terminal:
 # `ruby hashes_1.rb`
 
 foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
 
-# Write code that prints all of the 'keys' of the foods variable 
+
+# Write code that prints all of the 'keys' of the foods variable
 # you created above:
-# YOUR CODE HERE
+foods.each do |food, quantity|
+  puts food
+end
 
-# Write code that prints all of the 'values' of the foods variable 
+# Write code that prints all of the 'values' of the foods variable
 # you created above:
-# YOUR CODE HERE
+foods.each do |food, quantity|
+  puts quantity
+end
 
-# Write code that prints the value of the second food of the foods variable 
+# Write code that prints the value of the second food of the foods variable
 # you created above:
-# YOUR CODE HERE
+keys = foods.keys
+puts keys[1]
 
-# Write code that adds a food to the foods hash. 
+
+
+# Write code that adds a food to the foods hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+foods["tacos"] = 24
+foods.each do |food, quantity|
+	p "I need #{quantity} #{food}"
+end
 
 
 #-------------------
@@ -36,17 +46,33 @@ p zoo
 
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+email =
+{
+  "sender": "timmy@gmail.com",
+  "subject": "Your Car's Extended Warranty",
+  "inbox_no": 11254,
+  "content": "blahblahblah... blahblahblah",
+  "Cc": "wellsworth@outlook.com"
 
+}
 # Write code that prints your email hash to the terminal.
+email.each do |key, data|
+	p "#{key}: #{data}"
+end
 
-
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
+email_keys = email.keys
+email_keys.each do |key|
+  p key
+end
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
+email_values = email.values
+email_values.each do |value|
+  p value
+end
 
 
 #-------------------
@@ -74,7 +100,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -86,14 +112,42 @@ posts = [
 
 p posts
 p posts[0]
+puts ""
+puts ""
 
-
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the Terminal.
+emails = ["email_1", "email_2", "email_3"]
+
+emails = [
+  {
+    "sender": "gimmy@gmail.com",
+    "subject": "Your Bike's Extended Warranty",
+    "inbox_no": 11255,
+    "content": "I'm reaching out concerning...",
+    "Cc": "bobby@outlook.com"
+  },
+  {
+    "sender": "batman@gmail.com",
+    "subject": "Justice",
+    "inbox_no": 11256,
+    "content": "I will put you behind bars",
+    "Cc": "joker@outlook.com"
+  },
+  {
+    "sender": "lafawnduh@gmail.com",
+    "subject": "True Love",
+    "inbox_no": 11257,
+    "content": "Let's put a ring on it!",
+    "Cc": "napolean@outlook.com"
+  }
+]
+
+p emails
--- hashes_2.rb
+++ hashes_2.rb
@@ -1,45 +1,53 @@
 # 1- Using the following Hash
 
 states = {
-  "Oregon" => "OR",
-  "Alabama" => "AL",
-  "New Jersey" => "NJ",
-  "Colorado" => "CO"
+  "Oregon": "OR",
+  "Alabama": "AL",
+  "New Jersey": "NJ",
+  "Colorado": "CO"
 }
 
 # Use the each method to iterate through it and print out a sentence stating each state name and abbreviation
 #
 #
-#
+states.each do |name, abbr|
+	p "#{name} abbrieviated is #{abbr}"
+end
 
 
 # ======================================
 # 2- Using the following Hash
 
 birthdays = {
-  "Cindy" => "June 10",
-  "Rigo" => "March 31",
-  "Amos" => "July 21",
-  "Jeff" => "February 12"
+  "Cindy": "June 10",
+  "Rigo": "March 31",
+  "Amos": "July 21",
+  "Jeff": "February 12"
 }
 
 # Use the each method to iterate through it and print out a sentence stating each friends name and birthday
 #
 #
-#
+birthdays.each do |name, date|
+	p "#{name} has their birthday on #{date}"
+end
 
 
 # ======================================
 # 3- Using the following Hash
 
 login_statuses = {
-  "Cindy" => true,
-  "Rigo" => false,
-  "Amos" => true,
-  "Jeff" => true
+  "Cindy": true,
+  "Rigo": false,
+  "Amos": true,
+  "Jeff": true
 }
 
 # Use the each method to iterate through it and print ONLY the names of friends who are "logged in"
 #
 #
-#
+login_statuses.each do |name, status|
+	if status
+    p "#{name} is active"
+  end
+end
